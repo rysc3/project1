7a1ba0cfca468783364943436d21fd8573199535
ry <ry.scherbarth@gmail.com>
2024-03-21 17:21:35 -0600

    fix awful indentation

--- BigInteger.java
+++ BigInteger.java
@@ -1,128 +1,129 @@
 public class BigInteger {
-    IntegerNode head;
+  IntegerNode head;
 
-    /**
-     * Builds a new MyBigInteger from scratch using the string "number".
-     * 
-     * Runtime is Theta(n) because we only go over the string once.
-     * @param number the string to be converted into a BigInteger
-      */
-    public BigInteger(String number) {
-        char[] chars = number.toCharArray();
-        int len = chars.length;
-        int sign = (chars[0] == '-') ? -1 : 1;
-        int currValue = 0;
-        int multiplier = 1;
-        int numOfDigitsCounted = 0;
+  /**
+   * Builds a new MyBigInteger from scratch using the string "number".
+   * 
+   * Runtime is Theta(n) because we only go over the string once.
+   * 
+   * @param number the string to be converted into a BigInteger
+   */
+  public BigInteger(String number) {
+    char[] chars = number.toCharArray();
+    int len = chars.length;
+    int sign = (chars[0] == '-') ? -1 : 1;
+    int currValue = 0;
+    int multiplier = 1;
+    int numOfDigitsCounted = 0;
 
-        // create head node
-        this.head = new IntegerNode(sign);
-        IntegerNode currNode = this.head;
+    // create head node
+    this.head = new IntegerNode(sign);
+    IntegerNode currNode = this.head;
 
-        // iterate over A[1:len] because A[0] is the sign
-        for (int i = len-1; i >= 0; i--) {
-            if(chars[i] != '-') {
-                currValue += multiplier*Integer.parseInt(chars[i]+"");
-                multiplier *= 10;
-                numOfDigitsCounted++;
-            }
-            
-            if(numOfDigitsCounted == 4) { // added four numbers
-                // add a new node
-                currNode.addNextNode(currValue);
-                currNode = currNode.nextPos;
-                System.out.println(currNode.digits);
-                // reset variables
-                currValue = 0;
-                multiplier = 1;
-                numOfDigitsCounted = 0;
-            }
-        }
+    // iterate over A[1:len] because A[0] is the sign
+    for (int i = len - 1; i >= 0; i--) {
+      if (chars[i] != '-') {
+        currValue += multiplier * Integer.parseInt(chars[i] + "");
+        multiplier *= 10;
+        numOfDigitsCounted++;
+      }
 
-        if(currValue != 0) {
-            currNode.addNextNode(currValue);
-            System.out.println(currNode.digits);
-        }
+      if (numOfDigitsCounted == 4) { // added four numbers
+        // add a new node
+        currNode.addNextNode(currValue);
+        currNode = currNode.nextPos;
+        System.out.println(currNode.digits);
+        // reset variables
+        currValue = 0;
+        multiplier = 1;
+        numOfDigitsCounted = 0;
+      }
     }
 
-    /**
-     * Creates a new MyBigInteger by copying bigInt.
-     * 
-     * @param bigInt the MyBigInteger object to be copied
-      */
-    public BigInteger(BigInteger bigInt) {
-        IntegerNode currNode = new IntegerNode(bigInt.head.digits);
-        this.head = currNode;
-        
-        while(bigInt.head.nextPos != null) {
-            
-        }
-        
+    if (currValue != 0) {
+      currNode.addNextNode(currValue);
+      System.out.println(currNode.digits);
+    }
+  }
+
+  /**
+   * Creates a new MyBigInteger by copying bigInt.
+   * 
+   * @param bigInt the MyBigInteger object to be copied
+   */
+  public BigInteger(BigInteger bigInt) {
+    IntegerNode currNode = new IntegerNode(bigInt.head.digits);
+    this.head = currNode;
+
+    while (bigInt.head.nextPos != null) {
+
     }
 
-    /* ====================== OVERRIDDEN METHODS =================== */
+  }
 
-    /**
-     * Checks if two BigIntegers are equal by looping
-     * through each node in the lists.
-     * 
-     * @param obj the BigInteger being compared to "this".
-      */
-    @Override
-    public boolean equals(Object obj) {
-        if(obj == this) {
-            return true;
-        }
+  /* ====================== OVERRIDDEN METHODS =================== */
 
-        if(!(obj instanceof BigInteger)) {
-            return false;
-        }
-
-        BigInteger bigInt = (BigInteger) obj;
-        IntegerNode b1 = bigInt.head;
-        IntegerNode b2 = this.head;
-
-        while(b1 != null && b2 != null) {
-            if(b1.digits != b2.digits) return false;
-            b1 = b1.nextPos;
-            b2 = b2.nextPos;
-        }
-        
-        return b1 == null && b2 == null;
+  /**
+   * Checks if two BigIntegers are equal by looping
+   * through each node in the lists.
+   * 
+   * @param obj the BigInteger being compared to "this".
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) {
+      return true;
     }
 
-    @Override
-    public String toString() {
-        StringBuilder str = new StringBuilder();
-        IntegerNode currNode = this.head;
-
-        while(currNode != null) {
-            if(currNode.nextPos == null) {
-                str.append(String.format("[ %d ]",currNode.digits));
-            }
-            else {
-                str.append(String.format("[ %d ]->",currNode.digits));
-            }
-            currNode = currNode.nextPos;
-        }
-        return str.toString();
+    if (!(obj instanceof BigInteger)) {
+      return false;
     }
 
-    public static BigInteger add(BigInteger b1, BigInteger b2) {
-        return null;
+    BigInteger bigInt = (BigInteger) obj;
+    IntegerNode b1 = bigInt.head;
+    IntegerNode b2 = this.head;
+
+    while (b1 != null && b2 != null) {
+      if (b1.digits != b2.digits)
+        return false;
+      b1 = b1.nextPos;
+      b2 = b2.nextPos;
     }
+
+    return b1 == null && b2 == null;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder str = new StringBuilder();
+    IntegerNode currNode = this.head;
+
+    while (currNode != null) {
+      if (currNode.nextPos == null) {
+        str.append(String.format("[ %d ]", currNode.digits));
+      } else {
+        str.append(String.format("[ %d ]->", currNode.digits));
+      }
+      currNode = currNode.nextPos;
+    }
+    return str.toString();
+  }
+
+  public static BigInteger add(BigInteger b1, BigInteger b2) {
+    return null;
+  }
 }
 
 class IntegerNode {
-    int digits;
-    IntegerNode nextPos;
+  int digits;
+  IntegerNode nextPos;
 
-    public IntegerNode(int digits) {
-        this.digits = digits;
-        nextPos = null;
-    }
+  public IntegerNode(int digits) {
+    this.digits = digits;
+    nextPos = null;
+  }
 
-    public void addNextNode(int digits) {
-        this.nextPos = new IntegerNode(digits);
-    }
+  public void addNextNode(int digits) {
+    this.nextPos = new IntegerNode(digits);
+  }
 }
