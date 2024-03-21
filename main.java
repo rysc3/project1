c5ec2f5773ce27ed5c098820d85f7873e08c6084
xavier <xbarr@unm.edu>
2024-03-21 17:18:08 -0600

    initial commit

---
+++ main.java
@@ -0,0 +1,24 @@
+public class main {
+    public static void main(String[] args) {
+        BigInteger test1 = new BigInteger("-2048");
+        BigInteger test2 = new BigInteger("2048");
+        BigInteger test3 = new BigInteger("-2415763190");
+        // Tests from the word document
+        BigInteger test4 = new BigInteger("18364000098463281009282");
+        BigInteger test5 = new BigInteger("-9382361766839928276166829");
+        BigInteger test6 = new BigInteger("839947462729219484028272");
+        BigInteger test7 = new BigInteger("-839947462729219484028000");
+        BigInteger test8 = new BigInteger("-25634837829208474747382992822");
+        BigInteger test9 = new BigInteger("-6382927634646483929283733883");
+        System.out.println(test1);
+        System.out.println(test2);
+        System.out.println(test3);
+        System.out.println(test4);
+        System.out.println(test5);
+        System.out.println(test6);
+        System.out.println(test7);
+        System.out.println(test8);
+        System.out.println(test9);
+    }
+    
+}
