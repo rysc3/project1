b4264b4a0aae6f25841361fe3a410ef57726f712
maisydunlavy <mdunlavy@unm.edu>
2024-03-23 18:22:31 -0600

    broken again

--- main.java
+++ main.java
@@ -1,15 +1,15 @@
 public class main {
   public static void main(String[] args) {
-    MyBigInteger test1 = new MyBigInteger("-2048");
-    MyBigInteger test2 = new MyBigInteger("2048");
-    MyBigInteger test3 = new MyBigInteger("-2415763190");
-    // Tests from the word document
-    MyBigInteger test4 = new MyBigInteger("18364000098463281009282");
-    MyBigInteger test5 = new MyBigInteger("-9382361766839928276166829");
+    // MyBigInteger test1 = new MyBigInteger("-2048");
+    // MyBigInteger test2 = new MyBigInteger("2048");
+    // MyBigInteger test3 = new MyBigInteger("-2415763190");
+    // // Tests from the word document
+    // MyBigInteger test4 = new MyBigInteger("18364000098463281009282");
+    // MyBigInteger test5 = new MyBigInteger("-9382361766839928276166829");
     MyBigInteger test6 = new MyBigInteger("839947462729219484028272");
     MyBigInteger test7 = new MyBigInteger("-839947462729219484028000");
-    MyBigInteger test8 = new MyBigInteger("-25634837829208474747382992822");
-    MyBigInteger test9 = new MyBigInteger("-6382927634646483929283733883");
+    // MyBigInteger test8 = new MyBigInteger("-25634837829208474747382992822");
+    // MyBigInteger test9 = new MyBigInteger("-6382927634646483929283733883");
     // System.out.println(test1);
     // System.out.println(test2);
     // System.out.println(test3);
@@ -20,8 +20,20 @@ public class main {
      //System.out.println(test8);
     //System.out.println(test9);
 
-    MyBigInteger test10 = MyBigInteger.add(test8, test9);
-    System.out.println(test10);
+    MyBigInteger test10 = MyBigInteger.add(test6, test7);
+    System.out.println(test10.toString());
+    System.out.println(passingTest1());
   }
+
+  public static boolean passingTest1(){
+    MyBigInteger num1 = new MyBigInteger("18364000098463281009282");
+    MyBigInteger num2 = new MyBigInteger("-9382361766839928276166829");
+    MyBigInteger sum = MyBigInteger.add(num1, num2);
+    System.out.println(sum.toString());
+    System.out.println("-9363997766741464995157547");
+    return sum.toString().equals("-9363997766741464995157547");
+
+  }
+
 
 }
